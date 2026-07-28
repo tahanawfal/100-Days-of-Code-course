cde838f5590ed4ec206191354d601c53b62f2cfc
tahanawfal <da.tahanawfal@gmail.com>
2026-07-28 22:24:29 +0300

    finish day 16

--- Day 16/money_machine.py
+++ Day 16/money_machine.py
@@ -1,17 +1,14 @@
 class MoneyMachine:
-
-    CURRENCY = "$"
-
-    COIN_VALUES = {
-        "quarters": 0.25,
-        "dimes": 0.10,
-        "nickles": 0.05,
-        "pennies": 0.01
-    }
-
     def __init__(self):
         self.profit = 0
         self.money_received = 0
+        self.CURRENCY = "$"
+        self.COIN_VALUES = {
+            "quarters": 0.25,
+            "dimes": 0.10,
+            "nickles": 0.05,
+            "pennies": 0.01
+        }
 
     def report(self):
         """Prints the current profit"""
