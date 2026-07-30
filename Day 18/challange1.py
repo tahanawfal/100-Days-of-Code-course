156eed24cbfbab47b171abe840af034efaa578e0
tahanawfal <da.tahanawfal@gmail.com>
2026-07-30 16:59:42 +0300

    add files of day 18 and finish challange1

---
+++ Day 18/challange1.py
@@ -0,0 +1,9 @@
+import turtle as t
+
+tim = t.Turtle()
+
+tim.color("black")
+
+for _ in range(4):
+    tim.right(90)
+    tim.forward(100)
---
+++ Day 18/challange3.py
@@ -0,0 +1,6 @@
+
+import turtle as t
+
+tim = t.Turtle()
+
+########### Challenge 3 - Draw Shapes ########
